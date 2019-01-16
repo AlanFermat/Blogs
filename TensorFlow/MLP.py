01571e142c5b63655cef8ae9171698fd0924420c
AlanFermat <zy19@rice.edu>
2019-01-15 19:30:18 -0600

    add display funcs

--- TensorFlow/MLP.py
+++ TensorFlow/MLP.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as ran
 
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 
 def train_test_data(train_num, test_num):
 	x_train = mnist.train.images[:train_num,:]
@@ -45,6 +43,17 @@ correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
+def display_compare(num, i):
+    x_train = mnist.train.images[num,:].reshape(1,784)
+    y_train = mnist.train.labels[num,:]
+    label = y_train.argmax()
+    prediction = sess.run(y, feed_dict={x_: x_train}).argmax()
+    plt.title('Prediction: %d Label: %d' % (prediction, label))
+    plt.imshow(x_train.reshape([28,28]), cmap=plt.get_cmap('gray_r'))
+    plt.savefig("pic " + str(i) + ".png")
+    plt.show()
+
+
 train_step = 1000
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
@@ -52,11 +61,14 @@ with tf.Session() as sess:
 	for i in range(train_step+1):
 		sess.run(trainingAlg, feed_dict={x_: x_train, y_: y_train})
 		if i%100 == 0:
+			display_compare(ran.randint(0, 55000), i)
 			print('Training Step:' + str(i) + '  Accuracy =  ' 
 				+ str(sess.run(accuracy, feed_dict={x_: x_test, y_: y_test})) + '  Loss = ' + str(sess.run(loss, {x_: x_train, y_: y_train})))
 
 
 
 
+
+	
 
 
